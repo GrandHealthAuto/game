1784dffb7a5c4eb2deb7a1c50a9be50c1406dbc4
Matthias Richter <vrld@vrld.org>
2013-01-25 22:35:49 +0100

    Add entity template

---
+++ entities/template.lua
@@ -0,0 +1,21 @@
+local cls = Class{name = "Template", function(self)
+	Entities.register(self)
+	-- more init stuff
+end}
+
+function cls:update(dt)
+	-- behavior
+end
+
+function cls:draw()
+	-- appearance
+end
+
+function cls:onCollide(other, contact)
+	-- collision callback with other
+	Entities.remove(self)
+end
+
+-- etc.
+
+return cls
