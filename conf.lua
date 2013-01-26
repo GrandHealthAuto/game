e3dd52c87e8e3c24d8c6378aa1da36d98451576a
Matthias Richter <vrld@vrld.org>
2013-01-26 11:20:36 +0100

    add conf.lua

---
+++ conf.lua
@@ -0,0 +1,13 @@
+function love.conf(t)
+	t.title             = "Grand Health Auto"
+	t.author            = "GGJ13"
+	t.url               = "http://globalgamejam.org/2013/grand-health-auto"
+	t.identity          = "grand_health_auto"
+	--t.release           = true
+
+	t.screen.width      = 800
+	t.screen.height     = 600
+	t.screen.fullscreen = false
+	t.screen.fsaa       = 0
+	t.screen.vsync      = false
+end
