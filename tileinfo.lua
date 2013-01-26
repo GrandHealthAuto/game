8c8f0b35b91d51619023cde48615f800d018c590
Tilmann Hars <headchant@HeadPro.local>
2013-01-26 18:36:07 +0100

    map, tiles updated

--- tileinfo.lua
+++ tileinfo.lua
@@ -32,11 +32,14 @@ return {
     },
     {
         name = "sidewalk",
-		is_rescue_zone = true,
+        is_rescue_zone = true,
+    },
+    {
+        name = "hochhaus",
+        is_collision_tile = true
     },
 	{
-		name = "hochhaus",
-		is_collision_tile = true
+		name = "grass"
 	}
     
 }
