1cdef6f4dfd3613ff7eb0dfd14197d2ef124ceee
Xemle <xemle@phtagr.org>
2013-01-26 15:22:47 +0100

    Imprive car collision

--- entities/car.lua
+++ entities/car.lua
@@ -1,10 +1,9 @@
 local car = class{name = "Car", inherits = Entity.BaseEntity,
 	function (self, pos, angle, name)
                 Entity.BaseEntity.construct (self, pos, vector(32, 16))
-		self.visual = Image.car
+				self.visual = Image.car
                 self.name = name
                 self.angle = angle
-                self.debug = true
                 self.state = 'drive'
                 self.lastStateUpdate = love.timer.getMicroTime()
 
@@ -12,6 +11,9 @@ local car = class{name = "Car", inherits = Entity.BaseEntity,
                 self.rayCastLengthForward = 35
                 self.rayCastLengthSide = 20
                 self.rayCastAngleSide = 3.14159 / 4 -- 45°
+
+                self.hitList = {}
+                self.debug = false
 	end
 }
 
@@ -50,65 +52,90 @@ function car:updateState()
         self.state = 'halt' -- 10%
     end
 
-    if self.debug and oldState ~= self.state then
-        print(self.name .. ": Change state from " .. oldState .. " to " .. self.state)
+    if oldState ~= self.state then
+        self:log("Change state from " .. oldState .. " to " .. self.state)
     end
 end
 
-function car:detectCollition(hitList)
+function car:mergeList(one, another)
+    for i=1,#another do
+	table.insert(one, another[i])
+    end
+end
+
+function car:detectCollision(hitList)
+
+    local hitsForward = {}
     local headingForward = vector (math.cos(self.angle), math.sin(self.angle))
-    local headingLeft = vector (math.cos(self.angle - self.rayCastAngleSide), math.sin(self.angle - self.rayCastAngleSide))
-    local headingRight = vector (math.cos(self.angle + self.rayCastAngleSide), math.sin(self.angle + self.rayCastAngleSide))
-    
     local ray = self.pos + headingForward * self.rayCastLengthForward
-    State.game.world:rayCast(self.pos.x, self.pos.y, ray.x, ray.y, function (fixture, x, y, xn, yn, fraction) 
-        return self:worldRayCastCallback(fixture, x, y, xn, yn, fraction, hitList) 
+    State.game.world:rayCast(self.pos.x, self.pos.y, ray.x, ray.y, function (fixture, x, y, xn, yn, fraction)
+        return self:worldRayCastCallback(fixture, x, y, xn, yn, fraction, hitsForward)
     end )
+    self:mergeList(self.hitList, hitsForward)
+
+    -- If we have something in front of us we stop
+    if #hitsForward > 0 then
+	self:log("Front collision")
+	self.state = 'halt'
+    end
+
+    local hitsLeft = {}
+    local headingLeft = vector (math.cos(self.angle - self.rayCastAngleSide), math.sin(self.angle - self.rayCastAngleSide))
     local ray = self.pos + headingLeft * self.rayCastLengthSide
-    State.game.world:rayCast(self.pos.x, self.pos.y, ray.x, ray.y, function (fixture, x, y, xn, yn, fraction) 
-        return self:worldRayCastCallback(fixture, x, y, xn, yn, fraction, hitList) 
+    State.game.world:rayCast(self.pos.x, self.pos.y, ray.x, ray.y, function (fixture, x, y, xn, yn, fraction)
+        return self:worldRayCastCallback(fixture, x, y, xn, yn, fraction, hitsLeft)
     end )
+
+    local hitsRight = {}
+    local headingRight = vector (math.cos(self.angle + self.rayCastAngleSide), math.sin(self.angle + self.rayCastAngleSide))
     local ray = self.pos + headingRight * self.rayCastLengthSide
-    State.game.world:rayCast(self.pos.x, self.pos.y, ray.x, ray.y, function (fixture, x, y, xn, yn, fraction) 
-        return self:worldRayCastCallback(fixture, x, y, xn, yn, fraction, hitList) 
+    State.game.world:rayCast(self.pos.x, self.pos.y, ray.x, ray.y, function (fixture, x, y, xn, yn, fraction)
+        return self:worldRayCastCallback(fixture, x, y, xn, yn, fraction, hitsRight)
     end )
+
+    -- We turn right if something is on the left and on the right it is free.
+    -- Or vise versa. If there is something on the left and on the right we stop
+    if #hitsLeft > 0 and #hitsRight == 0 then
+	self:log("Left collision (" .. #hitsLeft .. ", " .. #hitsRight .. " right)")
+	self.angle = self.angle + 0.03
+    elseif #hitsRight > 0 and #hitsLeft == 0 then
+	self:log("Right collision (" .. #hitsLeft .. ", " .. #hitsRight .. " right)")
+	self.angle = self.angle - 0.03
+    elseif #hitsRight > 0 and #hitsLeft then
+	self:log("Left and right collision (" .. #hitsLeft .. ", " .. #hitsRight .. " right)")
+	self.state = 'halt'
+    end
+    self:mergeList(self.hitList, hitsLeft)
+    self:mergeList(self.hitList, hitsRight)
 end
 
 function car:update(dt)
 	-- behavior
         self:updateFromPhysics()
         self:updateState()
-        
+
         if self.state == 'left' then
             self.angle = self.angle - self.angleRotateSide
-            self.state = 'drive'
+            -- self.state = 'drive'
         elseif self.state == 'right' then
             self.angle = self.angle + self.angleRotateSide
-            self.state = 'drive'
+            -- self.state = 'drive'
         end
-            
-	local heading = vector (math.cos(self.angle), math.sin(self.angle))
 
-        local hitList = {}
-        self:detectCollition(hitList)
-        -- Collition detection
-        if #hitList == 0 then
-            --print("have " .. #hitList .. " hits")
-            self.velocity = heading * 64
-        else
-            self.velocity = vector(0, 0)
-            self.angle = self.angle + 0.3
-        end
+        self:detectCollision()
+
+	local heading = vector (math.cos(self.angle), math.sin(self.angle))
 
 	if self.state == 'fastdrive' then
             self.pos = self.pos + dt * self.velocity * 2
 	elseif self.state == 'drive' then
-            self.pos = self.pos + dt * self.velocity	
+            self.pos = self.pos + dt * self.velocity
 	elseif self.state == 'slowdrive' then
-            self.pos = self.pos + dt * self.velocity * 0.5	
+            self.pos = self.pos + dt * self.velocity * 0.5
         end
-	self.angle = self.angle + dt * self.angle_velocity
-        
+
+	-- self.angle = self.angle + dt * self.angle_velocity
+
 	self:updateToPhysics()
 end
 
@@ -127,7 +154,19 @@ function car:draw()
             local heading = vector (math.cos(self.angle + self.rayCastAngleSide), math.sin(self.angle + self.rayCastAngleSide))
             local ray = self.pos + heading * self.rayCastLengthSide
             love.graphics.line(self.pos.x, self.pos.y, ray.x, ray.y)
+
+	    for i = 1,#self.hitList do
+		local hit = self.hitList[i]
+		love.graphics.line(hit.x, hit.y-10, hit.x, hit.y+10)
+		love.graphics.line(hit.x-10, hit.y, hit.x+10, hit.y)
+	    end
         end
+end
+
+function car:log(msg)
+    if self.debug then
+	print(self.name .. ": " .. msg)
+    end
 end
 
 -- etc.
