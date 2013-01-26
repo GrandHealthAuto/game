772cd973c1bfd066618c661e3f610815423035c3
Peter Petermann <ppetermann80@googlemail.com>
2013-01-26 14:20:34 +0100

    only update data on load and on paging, not each iteration

--- states/highscore.lua
+++ states/highscore.lua
@@ -2,12 +2,15 @@ local Gui = require "Quickie"
 
 local highscorefetcher = require "highscore"
 local hs = highscorefetcher.new()
-
+local offset =0
 local st = GS.new()
 local mouse_hot, mouse_x, mouse_y
+local hsData = nil
 
 function st:enter()
 	mouse_hot, mouse_x, mouse_y = nil, nil, nil
+	hsData = hs:getHighscore(offset)
+
 end
 
 function st:update(dt)
@@ -20,28 +23,43 @@ function st:update(dt)
 		},
 	}
 	
-	Gui.group.push{ grow="right", size={ 300, 30}, pos={10,10} }
-	Gui.Label{text="Rank", size={100, 30}} Gui.Label{text="Name"} Gui.Label { text="Score"}
-	Gui.group.pop{}
-	
-	local hsData = hs:getHighscore(0)
-	if hsData then
-		for i, player in pairs(hsData) do
-			Gui.group.push{ grow="right", size={ 300, 30}, pos={10,0} }
-			Gui.Label{text=player["rank"], size={100, 30}} Gui.Label{text=player["name"]} Gui.Label { text=player["value"]}
-			Gui.group.pop{}
+		Gui.group.push{ grow="right", size={ 400, 30}}
+		
+			if Gui.Button{text="Previous Page"} then
+				offset = offset - 25
+				if offset < 0 then offset = 0 end
+				hsData = hs:getHighscore(offset)
+			end
+			
+			
+			if Gui.Button{text="Next Page"} then
+				offset = offset + 25
+				hsData = hs:getHighscore(offset)
+			end
+		
+		Gui.group.pop()
+		
+		Gui.group.push{ grow="right", size={ 300, 30}, pos={10,10} }
+			Gui.Label{text="Rank", size={100, 30}} Gui.Label{text="Name"} Gui.Label { text="Score"}
+		Gui.group.pop{}
+		
+		if hsData then
+			for i, player in pairs(hsData) do
+				Gui.group.push{ grow="right", size={ 300, 30}, pos={10,0} }
+					Gui.Label{text=player["rank"], size={100, 30}} Gui.Label{text=player["name"]} Gui.Label { text=player["value"], align="right"}
+				Gui.group.pop{}
+			end
 		end
-	end
-	
-	if Gui.Button{text="Back"} then
-		GS.switch(State.menu)
-    end
-
-	-- on mouse move -> set widget focus to mouse
-	if mouse_hot ~= Gui.mouse.getHot() then
-		Gui.keyboard.setFocus(Gui.mouse.getHot() or Gui.keyboard.getFocus())
-		mouse_hot = Gui.mouse.getHot()
-	end
+		
+		if Gui.Button{text="Back"} then
+			GS.switch(State.menu)
+		end
+		
+		-- on mouse move -> set widget focus to mouse
+		if mouse_hot ~= Gui.mouse.getHot() then
+			Gui.keyboard.setFocus(Gui.mouse.getHot() or Gui.keyboard.getFocus())
+			mouse_hot = Gui.mouse.getHot()
+		end
 	
 	Gui.group.pop{}
 end
