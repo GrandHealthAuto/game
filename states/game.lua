5ec88fef9b7132d16030eb6f8f831d34c4444020
Tilmann Hars <headchant@HeadPro.local>
2013-01-27 15:21:16 +0100

    sounds implemented

--- states/game.lua
+++ states/game.lua
@@ -99,7 +99,7 @@ function st:init()
 	-- pedestrians
 	Signal.register('pedestrian-killed', function (pedestrian)
 		-- hs:add(-100)
-		Sound.static["shout"..math.random(2)]:play()
+		Sound.static["shout"..math.random(7)]:play()
 		local v = Entity.victim(pedestrian.pos)
 		v.color = pedestrian.color
 		self.victims[v] = v
