b31d571c25259b34cdb1eac841b96bf7e36e5332
Peter Petermann <ppetermann80@googlemail.com>
2013-01-27 10:55:37 +0100

    add method to get saved highscore for specific user

--- highscore.lua
+++ highscore.lua
@@ -27,6 +27,12 @@ function Highscore:save()
 	return false
 end
 
+function Highscore:getSavedHighscore()
+	b,c,h = http.request(self.server .."get/", "name=" .. self.name
+	if not c == 200 then return false end
+	return b
+end
+
 --- returns table, containing tables with keys name/value/rank 
 function Highscore:getHighscore(offset)
 	b,c,h = http.request(self.server .. "load/", "offset=" .. offset)
