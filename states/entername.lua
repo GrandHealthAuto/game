e28b9dafa027184cb37a58db975905a89ec3c7b8
Peter Petermann <ppetermann80@googlemail.com>
2013-01-27 15:15:34 +0100

    same look as start menu

--- states/entername.lua
+++ states/entername.lua
@@ -16,7 +16,9 @@ function st:enter()
 	Gui.mouse.setActive(nil)
 end
 
+local t = 0
 function st:update(dt)
+	t = t + dt
 	love.graphics.setFont(Font.XPDR[16])
 
 	Gui.group.push{grow = "down", pos={SCREEN_WIDTH/2-Gui.group.default.size[1]/2,SCREEN_HEIGHT/2}}
@@ -35,8 +37,23 @@ function st:update(dt)
 end
 
 function st:draw()
-	love.graphics.draw(Image.logo, SCREEN_WIDTH/2, SCREEN_HEIGHT/4, 0, 2, 2, Image.logo:getWidth()/2, Image.logo:getHeight()/2)
-	love.graphics.setFont(Font.XPDR[16])
+	local w,h = Image.titlescreen:getWidth(), Image.titlescreen:getHeight()
+	love.graphics.setColor(20,20,20)
+	love.graphics.draw(Image.titlescreen,
+		math.sin(.2*t)*20 + SCREEN_WIDTH/2, math.cos(.3*t)*20 + SCREEN_HEIGHT/2,
+		math.sin(.02*t)*.05, 1.5,1.5, w/2,h/2)
+	love.graphics.setColor(255,255,255)
+
+	local lw,lh = Image.logo:getWidth(), Image.logo:getHeight()
+	love.graphics.setScissor(0,SCREEN_HEIGHT/4-lh*.5, SCREEN_WIDTH,lh)
+	love.graphics.draw(Image.titlescreen,
+		SCREEN_WIDTH/2, math.sin(.01*t)*h/2 + SCREEN_HEIGHT/4,
+		math.cos(.021*t)*.05, 1,1, w/2,h/2)
+	love.graphics.draw(Image.logo, SCREEN_WIDTH/4, SCREEN_HEIGHT/4, math.sin(.021*t)*.05, .8, .8, lw/2,lh/2)
+	love.graphics.setColor(122,0,10)
+	love.graphics.rectangle('fill', 0,SCREEN_HEIGHT/4-lh*.5-2, SCREEN_WIDTH,9)
+	love.graphics.rectangle('fill', 0,SCREEN_HEIGHT/4+lh*.5-7, SCREEN_WIDTH,7)
+	love.graphics.setScissor()
 	Gui.core.draw()
 end
 
